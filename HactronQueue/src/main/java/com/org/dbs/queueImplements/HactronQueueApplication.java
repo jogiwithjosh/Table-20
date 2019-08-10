9cecac4edfa69be334da50cd93361fc9d103ecff
venukuma <35678235+venukuma@users.noreply.github.com>
2019-08-10 12:28:01 +0530

    Add files via upload

---
+++ HactronQueue/src/main/java/com/org/dbs/queueImplements/HactronQueueApplication.java
@@ -0,0 +1,13 @@
+package com.org.dbs.queueImplements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HactronQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HactronQueueApplication.class, args);
+	}
+
+}
