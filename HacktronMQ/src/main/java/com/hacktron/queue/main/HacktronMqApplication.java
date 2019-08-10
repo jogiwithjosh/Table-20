7b8281b020f7e8a534dd5370a32e195696cfc882
Bhuvnesh Salwan <bhuvnesh.salwan@pasarpolis.com>
2019-08-10 11:58:01 +0530

    Initializing the SSprint Boot App.

---
+++ HacktronMQ/src/main/java/com/hacktron/queue/main/HacktronMqApplication.java
@@ -0,0 +1,13 @@
+package com.hacktron.queue.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacktronMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacktronMqApplication.class, args);
+	}
+
+}
---
+++ HacktronMQ/src/test/java/com/hacktron/queue/main/HacktronMqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hacktron.queue.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HacktronMqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
