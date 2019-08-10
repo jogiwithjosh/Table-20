b3c92d6985c674e4eaabf824ad2311695c7a87e1
Bhuvnesh Salwan <bhuvnesh.salwan@pasarpolis.com>
2019-08-10 14:15:25 +0530

    Added the delete queue, add element, list queues api endpoint.

--- HactronQueue/src/main/java/com/org/dbs/queueManager/QueueManager.java
+++ HactronQueue/src/main/java/com/org/dbs/queueManager/QueueManager.java
@@ -3,6 +3,7 @@ package com.org.dbs.queueManager;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 class QueueDetails {
@@ -30,6 +31,49 @@ public class QueueManager {
 		response.put("success", true);
 		response.put("message","topic created successfully.");
 		return response;
+	}
+	
+	public static HashMap<String, Object> deleteQueue(String topic){
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		if (queues.containsKey(topic)) {
+			queues.remove(topic);
+			response.put("success", true);
+			response.put("message", "topic deleted successfully");
+			return response;
+		}
+		response.put("success", false);
+		response.put("message", "topic doesn't exists");
+		return response;
+	}
+	
+	public static HashMap<String, Object> addElement(String topic, String element){
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		if (queues.containsKey(topic)) {
+			QueueDetails queue = queues.get(topic);
+			queue.queue.add(element);
+			response.put("success", true);
+			response.put("message", "message published successfully.");
+			return response;
+		}
+		response.put("success", false);
+		response.put("message", "topic doesn't exists");
+		return response;
+	}
+	
+	public static HashMap<String, Object> getQueues(){
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		if (queues.size() > 0) {
+			ArrayList<String> topics = new ArrayList<>(queues.keySet());
+			response.put("success", true);
+			response.put("queues", topics);
+			response.put("message", "list of queues.");
+			return response;
+		}
+		response.put("success", false);
+		response.put("queues", new ArrayList<String>());
+		response.put("messages", "no queues present.");
+		return response;
+		
 	}
 	 
 }
--- HactronQueue/src/main/java/com/org/dbs/queueOperations/QueueOperations.java
+++ HactronQueue/src/main/java/com/org/dbs/queueOperations/QueueOperations.java
@@ -4,7 +4,9 @@ import java.math.BigInteger;
 import java.util.HashMap;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.org.dbs.queueManager.QueueManager;
@@ -12,9 +14,34 @@ import com.org.dbs.queueManager.QueueManager;
 @RestController
 public class QueueOperations {
 
+	@GetMapping("/hello")
+	@ResponseBody
+	public String createQueue(@RequestParam String topic){
+		return topic;
+	}
+	
 	@GetMapping("/create-queue")
+	@ResponseBody
 	public HashMap<String, Object> createQueue(@RequestParam String topic, @RequestParam BigInteger size){
 		return QueueManager.createQueue(topic, size);
+	}
+	
+	@PostMapping("/delete-queue")
+	@ResponseBody
+	public HashMap<String, Object> deleteQueue(@RequestParam String topic, @RequestParam BigInteger size){
+		return QueueManager.deleteQueue(topic);
+	}
+	
+	@PostMapping("/add-element")
+	@ResponseBody
+	public HashMap<String, Object> addElement(@RequestParam String topic, @RequestParam String element){
+		return QueueManager.addElement(topic, element);
+	}
+	
+	@GetMapping("/list-topics")
+	@ResponseBody
+	public HashMap<String, Object> getQueues() {
+		return QueueManager.getQueues();
 	}
 	
 }
